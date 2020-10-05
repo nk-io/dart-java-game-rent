1d0befc6dd9972f3e75f972b207cee0d271c506e
a-brannen <brannen.a.ann@gmail.com>
2020-10-05 18:48:06 +0200

    Created the 4 types of membership linked to Customer; no connecting methods yet to Items

--- src/Customer.java
+++ src/Customer.java
@@ -8,7 +8,9 @@ import java.util.Collections;
 //Added sendAMessage method for User Story 9.1
 //Added findAllMessagesOfCurrentCustomer and showCustomersMessages methods for User Story 9.2
 //Added deleteAMessage method for VG
-public class Customer extends User{
+public class Customer extends User {
+    private Membership membership = Membership.NONE;
+    private int storeCredits = 0;
 
     Customer(String name, String password){
         super(name,password);
@@ -81,6 +83,40 @@ public class Customer extends User{
             else{
                 System.out.println("Returning to customer menu...");
             }
+        }
+    }
+
+    public boolean hasEnoughStoreCredits() {
+        if (this.storeCredits >= 5) {
+            return true;
+        }
+        return false;
+    }
+
+    // Getters
+    public Membership getMembership() {
+        return membership;
+    }
+    public int getStoreCredits() {
+        return storeCredits;
+    }
+
+    // Setters
+    public void setMembership(Membership membership) {
+        this.membership = membership;
+    }
+
+    public void setStoreCredits(int storeCredits) {
+        this.storeCredits = storeCredits;
+    }
+
+    public void incrementStoreCredits() {
+        this.storeCredits = this.storeCredits + this.membership.getStoreCredits();
+    }
+
+    public void useStoreCredits() {
+        if (hasEnoughStoreCredits()) {
+            this.storeCredits = this.storeCredits - 5;
         }
     }
 }
