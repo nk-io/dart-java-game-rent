0292f8742f86228a163fb7b4bb8d29a0d421b16b
Numan Korkmaz <heysemkorkmaz@gmail.com>
2020-10-13 00:10:15 +0200

    txt input and testing

--- src/DartController.java
+++ src/DartController.java
@@ -2,6 +2,10 @@
 // Methods are no longer statics
 // Added messageList for Epic Feature 9
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class DartController {
 
     private GameLibrary gameLibrary;
@@ -231,6 +235,66 @@ public class DartController {
             }
         } while(!(customerMenuOption == 13));
     }
+    //Customer;Name;Password
+    //Employee;Name;Password;BirthYear;Address;GrossSalary
+    //Game;Title;Genre;DailyRentFee
+    //Album;Title;Artist;Year;DailyRentFee
+    public String importDataFromATxt(String filePath){
+        try {
+            File myFile = new File("src/temp.txt");
+            FileReader fileReader = new FileReader(myFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            int lineCounter=0;
+            int customerCounter=0;
+            int employeeCounter=0;
+            int gameCounter=0;
+            int albumCounter=0;
+            while ((line = reader.readLine()) != null) {
+                lineCounter++;
+                String[] partsOfCurrentLine = line.split(";");
+                if(partsOfCurrentLine[0].equalsIgnoreCase("customer")){
+                    customerLibrary.registerCustomer(partsOfCurrentLine[1],partsOfCurrentLine[2]);
+                    customerCounter++;
+                }else if(partsOfCurrentLine[0].equalsIgnoreCase("employee")){
+                    employeeLibrary.registerEmployee(partsOfCurrentLine[1],partsOfCurrentLine[2],Integer.parseInt(partsOfCurrentLine[3]),partsOfCurrentLine[4], Double.parseDouble(partsOfCurrentLine[5]));
+                    employeeCounter++;
+                }else if(partsOfCurrentLine[0].equalsIgnoreCase("game")){
+                    gameLibrary.registerGame(partsOfCurrentLine[1], partsOfCurrentLine[2], Double.parseDouble(partsOfCurrentLine[3]));
+                    gameCounter++;
+                }else if(partsOfCurrentLine[0].equalsIgnoreCase("album")){
+                    albumLibrary.registerAlbum(partsOfCurrentLine[1], partsOfCurrentLine[2], Integer.parseInt(partsOfCurrentLine[3]), Double.parseDouble(partsOfCurrentLine[4]));
+                    albumCounter++;
+                }else{
+                    builder.append("Invalid input at line "+line+"\n");
+                }
+            }
+            if(customerCounter > 0){
+                builder.append(customerCounter + " customers ");
+            }
+            if (employeeCounter > 0){
+                builder.append(employeeCounter + " employees ");
+            }
+            if (gameCounter > 0){
+                builder.append(gameCounter + " games ");
+            }
+            if (albumCounter > 0){
+                builder.append(albumCounter + " albums ");
+            }
+            if(customerCounter+employeeCounter+gameCounter+albumCounter>0){
+                builder.append("\nTotal: "+(customerCounter+employeeCounter+gameCounter+albumCounter)+ " records have been imported successfully");
+            } else {
+                builder.append("No data to import!");
+            }
+            reader.close();
+            return builder.toString();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
 
     //initializes some test users
     //this method created for testing purposes
